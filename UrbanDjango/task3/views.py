f43d8eb7051faed5f6ca6f6e5488786500f3ebd6
thorny-rye <k.mytarev@bk.ru>
2025-01-05 13:32:47 +0300

    Add files via upload

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 def main_page(request):
-    return render(request, "third_task/platform.html")
+    mn_page = "Главная страница"
+    context = {'title': mn_page}
+    return render(request, "third_task/platform.html", context)
 
 def catalog(request):
-    return render(request, 'third_task/games.html', context=games)
+    title = "Игры"
+    context = {"title2": title,
+               'games': ["Atomic Heart", "Cyberpunk 2077", "PayDay 2"]}
+    return render(request, 'third_task/games.html', context)
 
 def purchases(request):
-    return render(request, 'third_task/cart.html')
+    cart = 'Корзина'
+    context = {"cart": cart}
+    return render(request, 'third_task/cart.html', context)
